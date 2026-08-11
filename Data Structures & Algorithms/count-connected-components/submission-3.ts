60379e160ec079a7cabe8347fc38a47d0dc0338f
Aditya Patel <patel.aditya2014@gmail.com>
2026-08-11 15:57:56 +0530

    Add: count-connected-components - submission-3

---
+++ Data Structures & Algorithms/count-connected-components/submission-3.ts
@@ -0,0 +1,52 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {number}
+     */
+    countComponents(n: number, edges: number[][]): number {
+        const parent = Array.from({length: n}, (_, i) => i);
+        const rank = Array(n).fill(0);
+        let components = n;
+
+
+        function find(x: number): number {
+            let root = x;
+
+            while(parent[root] !== root) {
+                root = parent[root];
+            }
+
+            while(parent[x] !== root) {
+                const next = parent[x];
+                parent[x] = root;
+                x = root;
+            }
+
+            return root;
+        }
+
+        function union(x: number, y: number): boolean {
+            const px = find(x);
+            const py = find(y);
+
+            if(px === py) return false;
+
+            if(rank[px] < rank[py]) parent[px] = py;
+            else if(rank[px] > rank[py]) parent[py] = px;
+            else {
+                parent[px] = py;
+                rank[py]++;
+            }
+
+            components--;
+            return true;
+        }
+
+        for(const [u, v] of edges) {
+            union(u, v);
+        }
+
+        return components;
+    }
+}
